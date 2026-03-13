c69235320225aef8bba47c542b6efc35aeccf578
ayeaon <315448508@qq.com>
2026-03-13 16:31:37 +0800

    feat：优化移动端卡片“批量删除”模式

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ except Exception:
     croniter = None
     CRONITER_AVAILABLE = False
 
-APP_VERSION = "v1.0.9"
+APP_VERSION = "v1.1.0"
 
 app = Flask(__name__)
 app.secret_key = os.getenv("SECRET_KEY", "autoiso-v2-secret-key")
